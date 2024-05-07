acff18c712d3aacca86f6782d7d4a78778a5ae7d
Evgeny <eugenysvirin@gmail.com>
2024-05-08 01:09:52 +0300

    Fix: client socket read

--- Client C++/untitled/clientmanager.cpp
+++ Client C++/untitled/clientmanager.cpp
@@ -74,8 +74,9 @@ void ClientManager::startRecieving() {
                 mainWindow->writeToChat(bytes.toStdString());
                 }
             }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
 }
 
 bool ClientManager::send(const QByteArray& data) {
